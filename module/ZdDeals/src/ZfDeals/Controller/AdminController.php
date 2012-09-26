8da4287431b8a8a523269db5249ff491062f47b4
messy <mail@weiss-daniel.de>
2012-09-26 16:52:42 +0200

    Module Skeleton

---
+++ module/ZdDeals/src/ZfDeals/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AdminController
+ *
+ * @author messy
+ */
+class AdminController {
+    //put your code here
+}
+
+?>
